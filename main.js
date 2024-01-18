e20ff6331d596a7c20d3d65e183e06901d66fe8d
John Schimenz <john.schimenz@gmail.com>
2024-01-18 06:17:35 -0600

    Weather app fetches current weather conditions and displays in the DOM

--- main.js
+++ main.js
@@ -3,26 +3,49 @@ const img = document.querySelector('img');
 
 // Fetch cat image from GIPHY
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=NetagrbBzo5BdC3fcYAZbfgyHMEbXGOc&s=cats', {mode: 'cors'})
-  .then(function(response) {
-    // Successful response :)
-    return response.json();
-  })
-  .then(function(response) {
-    img.src =(response.data.images.original.url);
-  })
-  /*
-  .catch(function(err) {
-    // Error :(
-    console.log(response);
-  });
+    .then(function(response) {
 
-/*
+        // Successful response
+        return response.json();
+    })
+
+    // Display cat image from GIPHY
+    .then(function(response) {
+        console.log(response);
+        img.src = response.data.images.original.url;
+    })
+
+    // Log to console if an error
+    .catch(function(err) {
+        // Error :(
+        console.log('Error: ' + err);
+    });
+
+// Select div containers for different parts of the weather data
+const city = document.querySelector('#city');
+const temperature = document.querySelector('#temperature');
+const windSpeed = document.querySelector('#wind-speed');
+const windDirection = document.querySelector('#wind-direction');
+const precipitation = document.querySelector('#precipitation');
+
 // Fetching weather data from Milwaukee
 fetch('https://api.weatherapi.com/v1/current.json?key=4b877b769355464b816210331231212&q=milwaukee', {mode: 'cors'})
-  .then(function(response) {
-    console.log('Got weather data');
-})
-  .catch(function(err) {
-    console.log('Did not get the weather data');
-  });
-  */
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(response) {
+        console.log(response);
+        
+        // DOM - displays all of the weather data
+        city.textContent = 'City: ' + response.location.name;
+        temperature.textContent = 'Temperature: ' + response.current.temp_f + ' degrees F';
+        windSpeed.textContent = 'Wind Speed: ' + response.current.wind_mph;
+        windDirection.textContent = 'Wind Direction: ' + response.current.wind_degree + ' degrees ' + response.current.wind_dir;
+        precipitation.textContent = 'Condition: ' + response.current.condition.text;
+    })
+
+    // Record error if weather data is not fetched properly
+    .catch(function(err) {
+        console.log('Did not get the weather data: ' + err);
+    });
